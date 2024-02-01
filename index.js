f10683aa88f0d48b4f0054315a75e6c89d8a43a8
Cobus Coetsee <cobus@matogen.com>
2024-02-01 13:33:41 +0200

    Update version number and readme docs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const yauzl = require('yauzl');
 
 // Define your CLI program
 program
-  .version('1.0.9')
+  .version('1.1.1')
   .description('Dropinbase CLI tool for starting a new Docker nginx project');
 
 // Define the 'start' command
